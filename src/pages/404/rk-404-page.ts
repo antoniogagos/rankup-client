a9f8a4fa114df44ef6cab422f7f211bf33e7fa04
Nacho García <nachoab@gmail.com>
2021-12-15 17:21:18 +0000

    some new empty pages

---
+++ src/pages/404/rk-404-page.ts
@@ -0,0 +1,29 @@
+import { LitElement, html, css } from 'lit';
+import { property } from 'lit/decorators.js';
+import { msg } from '@lit/localize';
+
+export class Rk404Page extends LitElement {
+  render() {
+    return html`
+      <h1>404 Missing</h1>
+      <a href="/">${msg('Back')}</a>
+    `;
+  }
+
+  static styles = [
+    css`
+      :host {
+        display: block;
+        background: beige;
+      }
+    `,
+  ];
+}
+
+customElements.define('rk-404-page', Rk404Page);
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'rk-404-page': Rk404Page;
+  }
+}
